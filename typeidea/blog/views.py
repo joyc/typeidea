2b4f572ec4c35757609e5620536bc1e7249431e0
joyc <joyc@live.jp>
2020-10-27 02:09:25 +0900

    change fbv to cbv

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,37 +1,85 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404
 
 from config.models import SideBar
-from .models import Post, Category
+from .models import Post, Category, Tag
+
+
+class CommonViewMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'sidebars': SideBar.get_all(),
+        })
+        context.update(Category.get_nav())
+        return context
+
+
+class IndexView(ListView):
+    queryset = Post.latest_posts()
+    paginate_by = 5
+    context_object_name = 'post_list'
+    template_name = 'blog/list.html'
+
+
+class CategoryView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        category = get_object_or_404(Category, pk=category_id)
+        context.update({
+            'category': category,
+        })
+        return context
+
+    def get_queryset(self):
+        """ 重写querset，根据分类过滤 """
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id)
+
+
+class TagView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(Tag, pk=tag_id)
+        context.update({
+            'tag': tag,
+        })
+        return context
+
+    def get_queryset(self):
+        """ 重写querset，根据标签过滤 """
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+        return queryset.filter(tag__id=tag_id)
 
 
 # 改为 class based view - ListView
-def post_list(request, category_id=None, tag_id=None):
-    tag = None
-    category = None
+# def post_list(request, category_id=None, tag_id=None):
+#     tag = None
+#     category = None
+#
+#     if tag_id:
+#         post_list, tag = Post.get_by_tag(tag_id)
+#     elif category_id:
+#         post_list, category = Post.get_by_category(category_id)
+#     else:
+#         post_list = Post.latest_posts()
+#
+#     context = {
+#         'category': category,
+#         'tag': tag,
+#         'post_list': post_list,
+#         'sidebars': SideBar.get_all(),
+#     }
+#     context.update(Category.get_nav())
+#     return render(request, 'blog/list.html', context=context)
 
-    if tag_id:
-        post_list, tag = Post.get_by_tag(tag_id)
-    elif category_id:
-        post_list, category = Post.get_by_category(category_id)
-    else:
-        post_list = Post.latest_posts()
-
-    context = {
-        'category': category,
-        'tag': tag,
-        'post_list': post_list,
-        'sidebars': SideBar.get_all(),
-    }
-    context.update(Category.get_nav())
-    return render(request, 'blog/list.html', context=context)
 
 
-class PostListView(ListView):
-    queryset = Post.latest_posts()
-    paginate_by = 1  # 分页设置的每页数量
-    context_object_name = 'post_list'  # 如果不设置则在模板中要使用 object_list 变量
-    template_name = 'blog/list.html'
 
 # 改为 class based view - DetailView
 # def post_detail(request, post_id=None):
@@ -48,6 +96,9 @@ class PostListView(ListView):
 #     return render(request, 'blog/detail.html', context=context)
 
 
-class PostDetailView(DetailView):
-    model = Post
+class PostDetailView(CommonViewMixin, DetailView):
+    queryset = Post.objects.filter(status=Post.STATUS_NORMAL)
     template_name = 'blog/detail.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'post_id'
+
