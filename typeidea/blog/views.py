96ed09116aeb1ff3e2f879e640a526055342b4f4
joyc <joyc@live.jp>
2020-10-27 01:36:33 +0900

    change to cbv

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from config.models import SideBar
 from .models import Post, Category
 
 
+# 改为 class based view - ListView
 def post_list(request, category_id=None, tag_id=None):
     tag = None
     category = None
@@ -25,15 +27,27 @@ def post_list(request, category_id=None, tag_id=None):
     return render(request, 'blog/list.html', context=context)
 
 
-def post_detail(request, post_id=None):
-    try:
-        post = Post.objects.get(id=post_id)
-    except Post.DoesNotExist:
-        post = None
-    context = {
-        'post': post,
-        'sidebars': SideBar.get_all(),
-    }
-    context.update(Category.get_nav())
+class PostListView(ListView):
+    queryset = Post.latest_posts()
+    paginate_by = 1  # 分页设置的每页数量
+    context_object_name = 'post_list'  # 如果不设置则在模板中要使用 object_list 变量
+    template_name = 'blog/list.html'
 
-    return render(request, 'blog/detail.html', context=context)
+# 改为 class based view - DetailView
+# def post_detail(request, post_id=None):
+#     try:
+#         post = Post.objects.get(id=post_id)
+#     except Post.DoesNotExist:
+#         post = None
+#     context = {
+#         'post': post,
+#         'sidebars': SideBar.get_all(),
+#     }
+#     context.update(Category.get_nav())
+#
+#     return render(request, 'blog/detail.html', context=context)
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
